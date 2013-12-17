f0be0c73f683d0880069213ac03fcb76b41cf554
ejackson <ejackson@microsoft.com>
2013-12-17 11:00:19 -0800

    Added regression tests

---
+++ Tst/Tests/API/LittleFuncLang/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace LittleFuncLang
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Microsoft.Formula.API;    
+    using Microsoft.Formula.API.Generators;
+    using Microsoft.Formula.API.Nodes;
+
+    class Program
+    {
+        private static string formulaFile;
+
+        static void Main(string[] args)
+        {
+            var asDir = new DirectoryInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            formulaFile = asDir.Parent.Parent.Parent.FullName + "\\LittleFuncLang.4ml";
+
+            List<ICSharpTerm> objects;
+            AST<Model> model;
+
+            if (LoadModelFromFile("Prog4", out objects))
+            {
+                ToAST("Prog4", objects, out model);
+
+                LittleFuncLang_Root.Let letExpr = null;
+                foreach (var o in objects)
+                {
+                    if (o is LittleFuncLang_Root.Let)
+                    {
+                        letExpr = (LittleFuncLang_Root.Let)o;
+                        break;
+                    }
+                }
+
+                if (letExpr == null)
+                {
+                    Console.WriteLine("Could not find a let expression");
+                }
+                else
+                {
+                    letExpr.@in = (LittleFuncLang_Root.IArgType_Let__2)letExpr.init;
+                }
+
+                ToAST("Prog4'", objects, out model);
+            }
+        }
+
+        private static bool ToAST(string modelName, List<ICSharpTerm> objects, out AST<Model> model)
+        {
+            var result = Factory.Instance.MkModel(
+                    modelName,
+                    "LittleFuncLang",
+                    objects,
+                    out model,
+                    null,
+                    "LittleFuncLang.4ml");
+
+            if (result)
+            {
+                model.Print(Console.Out);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Could not build model from object graph");
+                return false;
+            }
+        }
+
+        private static bool LoadModelFromFile(string model, out List<ICSharpTerm> objects)
+        {
+            objects = null;
+
+            InstallResult res;
+            var env = new Env();
+            if (!env.Install(formulaFile, out res) && res == null)
+            {
+                throw new Exception("Could not start installation");
+            }
+
+            foreach (var f in res.Flags)
+            {
+                Console.WriteLine("{0} ({1}, {2}) : {3} {4} : {5}",
+                    f.Item1.Node.Name,
+                    f.Item2.Span.StartLine,
+                    f.Item2.Span.StartCol,
+                    f.Item2.Severity,
+                    f.Item2.Code,
+                    f.Item2.Message);
+            }
+
+            if (!res.Succeeded)
+            {
+                Console.WriteLine("Could not install file; exiting");
+                return false;
+            }
+
+            Task<ObjectGraphResult> createTask;
+            if (!LittleFuncLang_Root.CreateObjectGraph(env, new ProgramName(formulaFile), "Prog4", out createTask))
+            {
+                throw new Exception("Could not start object graph creation");
+            }
+
+            createTask.Wait();
+            foreach (var f in createTask.Result.Flags)
+            {
+                Console.WriteLine("({0}, {1}) : {2} {3} : {4}",
+                    f.Span.StartLine,
+                    f.Span.StartCol,
+                    f.Severity,
+                    f.Code,
+                    f.Message);
+            }
+
+            if (!createTask.Result.Succeeded)
+            {
+                Console.WriteLine("Could not create object graph {0}; exiting", model);
+                return false;
+            }
+
+            objects = createTask.Result.Objects;
+            return true;
+        }
+    }
+}
---
+++ Tst/Tests/Parsers/ListParser/ListParser.cs
@@ -0,0 +1,238 @@
+﻿namespace ListParser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Microsoft.Formula.API;
+    using Microsoft.Formula.API.ASTQueries;
+    using Microsoft.Formula.API.Nodes;
+    using Microsoft.Formula.API.Plugins;
+    using Microsoft.Formula.Compiler;
+    using Microsoft.Formula.Common;
+
+    public class ListParser : IQuoteParser
+    {
+        private enum ParserState { None, Nat, Id };
+
+        private static readonly Tuple<string, CnstKind>[] noSettings = new Tuple<string, CnstKind>[0];
+        private static readonly AST<Domain> listDomain;
+        private const string consName = "Cons";
+        private const string nilName = "NIL";
+        private const char idPrefix = '$';
+        private const string idPrefixStr = "$";
+        private const string listDomainStr =
+        @"
+        domain Lists 
+        {{
+            {0} ::= new (left: any {0} + Natural + {{ {1} }}, right: any {0} + Natural + {{ {1} }}).
+        }}
+        ";
+
+        static ListParser()
+        {
+            var progName = new ProgramName("env:///temp.4ml");
+            var progText = string.Format(listDomainStr, consName, nilName);
+            var task = Factory.Instance.ParseText(progName, progText);
+            task.Wait();
+            if (!task.Result.Succeeded)
+            {
+                throw new Exception("Could not parse domain definition");
+            }
+
+            var query = new NodePred[] { 
+              NodePredFactory.Instance.Star, 
+              NodePredFactory.Instance.MkPredicate(NodeKind.Domain) };
+            listDomain = (AST<Domain>)task.Result.Program.FindAny(query);
+        }
+
+        public string UnquotePrefix
+        {
+            get { return idPrefixStr; }
+        }
+
+        public AST<Domain> SuggestedDataModel
+        {
+            get { return listDomain; }
+        }
+
+        public IEnumerable<Tuple<string, CnstKind>> SuggestedSettings
+        {
+            get { return noSettings; }
+        }
+
+        public ListParser()
+        {
+        }
+
+        public IQuoteParser CreateInstance(
+                            AST<Node> module,
+                            string collectionName,
+                            string instanceName)
+        {
+            return new ListParser();
+        }
+
+        public bool Parse(
+                Configuration config,
+                Stream quoteStream,
+                SourcePositioner positioner,
+                out AST<Node> results,
+                out List<Flag> flags)
+        {
+            results = null;
+            flags = new List<Flag>();
+            var lines = new LinkedList<string>();
+            var revList = new LinkedList<AST<Node>>();
+            using (var sr = new StreamReader(quoteStream))
+            {
+                while (!sr.EndOfStream)
+                {
+                    lines.AddLast(sr.ReadLine());
+                }
+            }
+
+            char c;
+            string token = string.Empty;
+            var state = ParserState.None;
+            int lineNum = 0, colNum = 0;
+            int sL = 0, sC = 0;
+            foreach (var line in lines)
+            {
+                for (colNum = 0; colNum < line.Length; ++colNum)
+                {
+                    c = line[colNum];
+                    if (char.IsDigit(c))
+                    {
+                        token += c;
+                        if (state == ParserState.None)
+                        {
+                            state = ParserState.Nat;
+                            sL = lineNum;
+                            sC = colNum;
+                        }
+                    }
+                    else if (c == idPrefix)
+                    {
+                        if (state == ParserState.None)
+                        {
+                            token = idPrefixStr;
+                            state = ParserState.Id;
+                            sL = lineNum;
+                            sC = colNum;
+                        }
+                        else 
+                        {
+                            flags.Add(new Flag(
+                                SeverityKind.Error,
+                                positioner.GetSourcePosition(lineNum, colNum, lineNum, colNum),
+                                Constants.QuotationError.ToString("Unexpected character " + c),
+                                Constants.QuotationError.Code));
+                            return false;
+                        }
+                    }
+                    else if (c == ' ' || c == '\t')
+                    {
+                        if (state == ParserState.Nat)
+                        {
+                            Contract.Assert(token.Length > 0);
+                            Rational r;
+                            Rational.TryParseFraction(token, out r);
+                            revList.AddFirst(Factory.Instance.MkCnst(r, positioner.GetSourcePosition(sL, sC, lineNum, colNum - 1)));
+                            token = string.Empty;
+                        }
+                        else if (state == ParserState.Id)
+                        {
+                            if (token.Length < 2)
+                            {
+                                flags.Add(new Flag(
+                                    SeverityKind.Error,
+                                    positioner.GetSourcePosition(lineNum, colNum, lineNum, colNum),
+                                    Constants.QuotationError.ToString("Bad id"),
+                                    Constants.QuotationError.Code));
+                                return false;
+                            }
+
+                            revList.AddFirst(Factory.Instance.MkId(token, positioner.GetSourcePosition(sL, sC, lineNum, colNum - 1)));
+                            token = string.Empty;
+                        }
+
+                        state = ParserState.None;
+                    }
+                    else
+                    {
+                        flags.Add(new Flag(
+                            SeverityKind.Error,
+                            positioner.GetSourcePosition(lineNum, colNum, lineNum, colNum),
+                            Constants.QuotationError.ToString("Unexpected character " + c),
+                            Constants.QuotationError.Code));
+                        return false;
+                    }
+                }
+
+                if (state == ParserState.Nat)
+                {
+                    Contract.Assert(token.Length > 0);
+                    Rational r;
+                    Rational.TryParseFraction(token, out r);
+                    revList.AddFirst(Factory.Instance.MkCnst(r, positioner.GetSourcePosition(sL, sC, lineNum, colNum)));
+                }
+                else if (state == ParserState.Id)
+                {
+                    if (token.Length < 2)
+                    {
+                        flags.Add(new Flag(
+                            SeverityKind.Error,
+                            positioner.GetSourcePosition(lineNum, colNum, lineNum, colNum),
+                            Constants.QuotationError.ToString("Bad id"),
+                            Constants.QuotationError.Code));
+                        return false;
+                    }
+
+                    revList.AddFirst(Factory.Instance.MkId(token, positioner.GetSourcePosition(sL, sC, lineNum, colNum)));
+                }
+
+                token = string.Empty;
+                state = ParserState.None;
+                ++lineNum;
+            }
+
+            if (revList.Count == 0)
+            {
+                results = Factory.Instance.MkId(nilName, positioner.GetSourcePosition(0, 0, 0, 0));
+            }
+            else 
+            {
+                foreach (var item in revList)
+                {
+                    if (results == null)
+                    {
+                        results = item;
+                    }
+                    else
+                    {
+                        results = Factory.Instance.AddArg(
+                                    Factory.Instance.MkFuncTerm(Factory.Instance.MkId(consName, item.Node.Span), item.Node.Span),
+                                    results);
+                        results = Factory.Instance.AddArg((AST<FuncTerm>)results, item, false);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public bool Render(
+                Configuration config,
+                TextWriter writer,
+                AST<Node> ast,
+                out List<Flag> flags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

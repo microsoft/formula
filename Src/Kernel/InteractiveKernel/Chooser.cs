ab5bac55db8b7596197f74bb2827f92485aa1cfa
saj122 <sajttu22@gmail.com>
2022-02-21 08:04:20 -0600

    Added kernel for running formula in jupyter notebooks.

---
+++ Src/Kernel/InteractiveKernel/Chooser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Formula.CommandLine;
+
+namespace Microsoft.Jupyter.Core
+{
+    public class Chooser : IChooser
+    {
+        public int _choice = -1;
+        public Chooser()
+        {
+            Interactive = true;
+        }
+
+        public bool Interactive { get; set; }
+
+        public bool GetChoice(out DigitChoiceKind choice)
+        {
+            if(_choice != -1)
+            {
+                choice = (DigitChoiceKind)_choice;
+                return true;
+            }
+            choice = DigitChoiceKind.Zero;
+            return false;
+        }
+
+        public void SetChoice(int num)
+        {
+            _choice = num;
+        }
+    }
+}
---
+++ Src/Kernel/InteractiveKernel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using static Microsoft.Jupyter.Core.Constants;
+using Microsoft.Extensions.DependencyInjection;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace Microsoft.Jupyter.Core
+{
+    class InteractiveKernel
+    {
+        public static void Init(ServiceCollection serviceCollection) =>
+            serviceCollection
+                .AddSingleton<IExecutionEngine, KernelEngine>();
+
+        internal static CommandOption? FormulaOption;
+
+        public static int Main(string[] args)
+        {
+            var app = new KernelApplication(
+                PROPERTIES,
+                Init
+            );
+            CommandOption formulaInstallOption;
+
+            return app
+                .WithDefaultCommands()
+                .WithKernelSpecResources<InteractiveKernel>(
+                    new Dictionary<string, string>
+                    {
+                        ["4ml-icon.png"] = "InteractiveKernel.res.4ml-icon.png"
+                    }
+                )
+                .Execute(args);
+        }
+    }
+}
+

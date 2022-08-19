97b401702736d532971164b1a4c3f3fd8c2bf42d
saj122 <sajttu22@gmail.com>
2022-08-18 19:43:01 -0500

    Removed command line fixture into a single file to avoid clashes in namespace.

--- Src/Tests/SolveTests.cs
+++ Src/Tests/SolveTests.cs
@@ -117,23 +117,4 @@ namespace Tests
             Assert.True(SolvePartialModel("pm"));
         }
     }
-
-    public class CommandInterfaceFixture : IDisposable
-    {
-        private readonly CommandLineProgram.ConsoleChooser _chooser;
-        private readonly CommandLineProgram.ConsoleSink _sink;
-        public CommandInterface _ci { get; private set; }
-
-        public CommandInterfaceFixture()
-        {
-            _chooser = new CommandLineProgram.ConsoleChooser();
-            _sink = new CommandLineProgram.ConsoleSink();
-            _ci = new CommandInterface(_sink, _chooser);
-        }
-
-        public void Dispose()
-        {
-            _ci.Cancel();
-        }
-    }
 }

a8a37466460d0e57ac817e9b6c410944e3337e8b
Mark Wutka <mark.wutka@vanderbilt.edu>
2022-08-23 13:32:20 -0500

    Added example plugin project

---
+++ Src/Examples/FormulaPluginTest/FormulaPluginTest/MathFunctions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Formula.Common;
+
+namespace FormulaPluginTest;
+
+using Microsoft.Formula.Common.Terms;
+
+public class SineFunction : OpPluginFunc
+{
+    public static BaseSortKind[] ArgTypes = new[] {BaseSortKind.Real};
+
+    public override string GetName()
+    {
+        return "sin";
+    }
+    
+    public override BaseSortKind[] GetArgTypes()
+    {
+        return ArgTypes;
+    }
+
+    public override BaseSortKind GetReturnType()
+    {
+        return BaseSortKind.Real;
+    }
+
+    public override Rational Evaluate(Rational[] args)
+    {
+        return new Rational(Math.Sin(RationalToDouble(args[0])));
+
+    }
+}
